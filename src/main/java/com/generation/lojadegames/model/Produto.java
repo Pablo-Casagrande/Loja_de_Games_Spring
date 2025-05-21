11bc92fb284e3080283cdde30f5e39d21105993c
unknown <furtadocasagrandepablo@gmail.com>
2025-05-21 14:59:44 -0300

    :sparkles: Criação da Model, Repository e Controller

--- src/main/java/com/generation/lojadegames/model/Produto.java
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -3,6 +3,7 @@ package com.generation.lojadegames.model;
 import java.math.BigDecimal;
 
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
